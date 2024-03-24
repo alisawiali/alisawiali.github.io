74fe12adcbf7ad51e001616d4eda95cfadf40f7a
alisawiali <alisawiali70@gmail.com>
2024-03-24 16:56:40 +0100

    Nue Portfolio

--- frontent/src/UL/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Navbar } from "../components/Navbar/Navbar.jsx";
-export { default as Intro } from "../components/Intro/Intro.jsx";
-export { default as FloatingDiv } from "../components/FloatingDiv/FloatingDiv.jsx";
-export { default as Services } from "../components/Services/Services.jsx";
-export { default as Experience } from "../components/Experience/Experience.jsx";
-export { default as Protfolio } from "../components/Protfolio/Protfolio.jsx";
-export { default as ModalContact } from "../components/Contact/ContactModal.jsx";
-export { default as Footer } from "../components/Footer/Footer.jsx";
